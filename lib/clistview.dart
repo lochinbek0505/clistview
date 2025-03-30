ab2a8dade4f383fa58a02c600ddc90c460652e37
Yengilmas <lbek458@gmal.com>
2025-03-30 17:00:38 +0500

    test library is active

--- lib/clistview.dart
+++ lib/clistview.dart
@@ -1,5 +1,30 @@
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+library clistview;
+export 'package:clistview/clistview.dart';
+import 'package:flutter/material.dart';
+
+class CustomListview extends StatelessWidget {
+  VoidCallback onTap;
+  Widget child;
+  int length;
+
+
+  CustomListview(this.onTap, this.child, this.length);
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Container(
+        alignment: Alignment.center,
+        child: ListView.builder(
+          itemBuilder: (context, index) {
+            return GestureDetector(
+              onTap: () => onTap,
+              child: child,
+            );
+          },
+          itemCount: length,
+        ),
+      ),
+    );
+  }
 }
